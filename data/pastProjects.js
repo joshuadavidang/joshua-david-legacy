8aee9d88ceb7e5f1b46ed6db6c7af9e2797b142e
joshuadavidang <chunxiong_@outlook.sg>
2022-08-05 10:11:16 +0800

    content update

--- data/pastProjects.js
+++ data/pastProjects.js
@@ -1,66 +1,65 @@
 export const pastProjects = [
   {
-    name: "Sayocode",
-    description:
-      "Final year project at Singapore Polytechnic, Sayocode automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
-    year: 2019,
-    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
-  },
-  {
-    name: "Sayocode",
-    description:
-      "Final year project at Singapore Polytechnic, Sayocode automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
-    year: 2019,
-    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
-  },
-  {
-    name: "Sayocode",
-    description:
-      "Final year project at Singapore Polytechnic, Sayocode automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
-    year: 2019,
-    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
-  },
-  {
-    name: "Sayocode",
-    description:
-      "Final year project at Singapore Polytechnic, Sayocode automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
-    year: 2019,
-    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
-  },
-  {
-    name: "Sayocode",
-    description:
-      "Final year project at Singapore Polytechnic, Sayocode automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
-    year: 2019,
-    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
-  },
-
-  {
-    name: "Sayocode",
-    description:
-      "Final year project at Singapore Polytechnic, Sayocode automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
-    year: 2019,
-    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
-  },
-  {
     name: "Still Young",
     description:
-      "Cross-platform mobile app that serves as a centralized platform for young mums to look up information during their confinement period. With more than 200 downloads, it's now available on App Store & Google Play Store.",
+      "Mobile app that serves as a centralized platform for young mums to look up information during their confinement period. With more than 200 downloads, it's now available on App Store & Google Play Store.",
     year: 2019,
     techStack: ["Ionic", "Express.js"],
   },
   {
-    name: "Still Young",
+    name: "Sayocode",
     description:
-      "Cross-platform mobile app that serves as a centralized platform for young mums to look up information during their confinement period. With more than 200 downloads, it's now available on App Store & Google Play Store.",
+      "Automates the process of app creation through a seamless method of dragging and dropping components directly to a mobile screen of your choice.",
     year: 2019,
-    techStack: ["Ionic", "Express.js"],
+    techStack: ["TailwindCss", "React.js", "Ionic", "Express.js", "MongoDB"],
   },
   {
-    name: "Still Young",
+    name: "Lorem, ipsum dolor",
     description:
-      "Cross-platform mobile app that serves as a centralized platform for young mums to look up information during their confinement period. With more than 200 downloads, it's now available on App Store & Google Play Store.",
-    year: 2019,
-    techStack: ["Ionic", "Express.js"],
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
+  },
+  {
+    name: "Lorem, ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
+  },
+  {
+    name: "Lorem, ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
+  },
+  {
+    name: "Lorem, ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
+  },
+  {
+    name: "Lorem, ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
+  },
+  {
+    name: "Lorem, ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
+  },
+  {
+    name: "Lorem, ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum in repellendus tempore placeat odit soluta, quo obcaecati ratione voluptas autem laboriosam voluptatibus eveniet repudiandae perferendis maiores. Atque adipisci maiores iure?",
+    year: 2555,
+    techStack: ["Lorem", "ipsum", "dolar", "sit"],
   },
 ];
