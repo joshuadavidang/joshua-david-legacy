e25f4364f69cd469180afb232b15b53b1f523c0b
joshuadavidang <chunxiong_@outlook.sg>
2022-12-03 23:59:51 +0800

    added e2e testing

---
+++ cypress/e2e/Navigation.cy.ts
@@ -0,0 +1,6 @@
+describe("Navigation", () => {
+  it("should navigate to landing page", () => {
+    cy.visit("http://localhost:3000/");
+    cy.url().should("include", "/Landing/LandingPage");
+  });
+});
