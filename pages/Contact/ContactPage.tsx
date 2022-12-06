7d20f9768ae571f14701ecc40142cf18d532b081
joshuadavidang <joshuadavidang@outlook.sg>
2022-12-06 17:53:31 +0800

    removed form

--- pages/Contact/ContactPage.tsx
+++ pages/Contact/ContactPage.tsx
@@ -6,48 +6,48 @@ import Form from "../../components/Form/index";
 
 const ContactPage = () => {
   return (
-    <main className="flex md:flex-row flex-col md:gap-32 justify-center px-10 items-center h-screen w-screen text-center md:p-0 pt-56 pb-20 overflow-scroll">
+    <main className="flex md:flex-row flex-col md:gap-32 justify-center items-center h-screen w-screen text-center">
       <div className="md:w-3/12 w-8/12">
         <Image src={Coffee} alt="coffee" />
+      </div>
 
-        <div className="pt-10">
-          <p className="text-blue1 dark:text-blue1 text-2xl md:text-4xl font-sen tracking-tight">
-            stay connected
-          </p>
+      <div className="pt-10">
+        <p className="text-blue1 dark:text-blue1 text-2xl md:text-4xl font-sen tracking-tight">
+          stay connected
+        </p>
 
-          <p className="hover:bg-lightGray pt-3 dark:hover:bg-gray rounded cursor-pointer">
-            <a href="mailto:joshuadavidang@outlook.sg">
-              joshuadavidang@outlook.sg
+        <p className="hover:bg-lightGray pt-3 dark:hover:bg-gray rounded cursor-pointer">
+          <a href="mailto:joshuadavidang@outlook.sg">
+            joshuadavidang@outlook.sg
+          </a>
+        </p>
+        <div className="flex flex-row justify-center gap-0.5 pt-3">
+          <span className="hover:bg-lightGray dark:hover:bg-gray p-2 rounded cursor-pointer">
+            <Link href="https://github.com/joshuadavidang" target="_blank">
+              <FaGithub size={30} className="cursor-pointer" />
+            </Link>
+          </span>
+
+          <span className="hover:bg-lightGray dark:hover:bg-gray p-2 rounded cursor-pointer">
+            <Link
+              href="https://linkedin.com/in/joshuadavidang/"
+              target="_blank"
+            >
+              <FaLinkedin size={30} className="cursor-pointer" />
+            </Link>
+          </span>
+
+          <span className="hover:bg-lightGray dark:hover:bg-gray p-2 rounded cursor-pointer">
+            <a download href="/Joshua David.pdf">
+              <FaFileAlt size={30} className="cursor-pointer" />
             </a>
-          </p>
-          <div className="flex flex-row justify-center gap-0.5 pt-3">
-            <span className="hover:bg-lightGray dark:hover:bg-gray p-2 rounded cursor-pointer">
-              <Link href="https://github.com/joshuadavidang" target="_blank">
-                <FaGithub size={30} className="cursor-pointer" />
-              </Link>
-            </span>
-
-            <span className="hover:bg-lightGray dark:hover:bg-gray p-2 rounded cursor-pointer">
-              <Link
-                href="https://linkedin.com/in/joshuadavidang/"
-                target="_blank"
-              >
-                <FaLinkedin size={30} className="cursor-pointer" />
-              </Link>
-            </span>
-
-            <span className="hover:bg-lightGray dark:hover:bg-gray p-2 rounded cursor-pointer">
-              <a download href="/Joshua David.pdf">
-                <FaFileAlt size={30} className="cursor-pointer" />
-              </a>
-            </span>
-          </div>
+          </span>
         </div>
       </div>
 
-      <div className="md:w-fit w-10/12 md:mt-0 mt-10">
+      {/* <div className="md:w-fit w-10/12 md:mt-0 mt-10">
         <Form />
-      </div>
+      </div> */}
     </main>
   );
 };
