e3c905dd8466140df505dbd4bdcc4791a9b88229
joshuadavidang <joshuadavidang@outlook.sg>
2022-12-30 17:27:43 +0800

    bug fixed

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar: NextPage<NavBarProps> = (props) => {
   return (
     <div
       className={`bg-white dark:bg-midnight flex md:justify-end md:flex-row md:gap-5 md:pr-12 md:p-6 w-screen md:items-center 
-      flex-col items-end p-1.5 pr-6 ${burgerState ? styles.animate : ""}`}
+      flex-col items-end p-1.5 pr-6`}
     >
       <span className="hidden md:block">
         <DarkMode />
