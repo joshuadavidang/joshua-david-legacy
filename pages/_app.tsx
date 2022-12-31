d5681d42e9ca91fe22264d9ef529f04845190870
joshuadavidang <joshuadavidang@outlook.sg>
2022-12-31 11:18:36 +0800

    fix: bug fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import Layout from "./index";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
+import theme from "./_theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
