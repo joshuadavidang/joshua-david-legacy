69f58980207dbb038dd5434f38bb789f89f1c97d
joshuadavidang <joshuadavidang@outlook.sg>
2023-06-09 14:14:50 +0800

    feat: handle 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Box, useColorMode } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import { useColorMode } from '@chakra-ui/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Lost from '@/images/Lost.svg';
@@ -9,11 +8,7 @@ import { ButtonIcon } from '@/components/Button/ButtonIcon';
 
 export default function Custom404() {
   const { colorMode } = useColorMode();
-  const router = useRouter();
-  const handleClick = (e: any) => {
-    e.preventDefault();
-    router.push('/discover');
-  };
+
   return (
     <>
       <Head>
@@ -44,30 +39,6 @@ export default function Custom404() {
           </Link>
         </div>
       </div>
-
-      {/* <Box className="flex flex-col md:flex-row justify-center items-center md:space-x-48 w-screen h-screen pt-12 md:gap-0 gap-12">
-        <div className="px-16">
-          <Image src={Lost} alt="404-hero" width="350" />
-        </div>
-        <div className="flex flex-col gap-2 justify-start items-start">
-          <h1
-            className={`${colorMode === 'dark' ? 'text-white' : 'text-black'}`}
-          >
-            Oh No! Error 404
-          </h1>
-          <h3>Page Not Found</h3>
-          <Link href="/discover" className="pt-6">
-            <ButtonIcon
-              size="md"
-              variant="solid"
-              isDisabled={false}
-              colorScheme={colorMode == 'dark' ? 'gray' : 'telegram'}
-            >
-              Back To Home
-            </ButtonIcon>
-          </Link>
-        </div>
-      </Box> */}
     </>
   );
 }
