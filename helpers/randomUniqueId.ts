fe08f06e22416d7d6d5d16e0ad39706fae076494
joshuadavidang <joshuadavidang@outlook.sg>
2023-08-03 06:52:09 +0800

    refactor: return type value for helpers

--- helpers/randomUniqueId.ts
+++ helpers/randomUniqueId.ts
@@ -4,7 +4,7 @@
  * @returns a string
  */
 
-const randomUniqueId = () => {
+const randomUniqueId = (): string => {
   const timeStamp = Date.now().toString(36);
   const randomString = Math.random().toString(36);
   return timeStamp + randomString + Math.floor(Math.random() * 999);
